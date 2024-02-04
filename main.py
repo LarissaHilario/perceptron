1b9c7fdf60c82ea378d9798c6fed56933e8d44e3
Larissa Hilario Clemente <larihid1@gmail.com>
2024-02-04 17:24:45 -0600

    feat: fixed weights and delta

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import numpy as np
 matrix_data = []
 y_desired = []
 weights = []
-learning_rate = 0.01 
+learning_rate = 1
 epochs = 4
 bias = 1
 error_norms = []
 u_values = []
 y_calculadas = []
+
+name_file = './data/E2.csv'
 
 def read_data(name_file):
     global matrix_data, weights
@@ -21,14 +23,13 @@ def read_data(name_file):
             row_numbers = [float(number) for number in row[:-1]]  
             matrix_data.append(row_numbers)
             y_desired.append(int(row[-1]))
-            entry_weights = create_weights(len(row))
+            entry_weights = create_weights(np.array(matrix_data).shape[1] + 1)
             weights.append(entry_weights)
     matrix_data_w0 = np.insert(np.array(matrix_data), 0, 1, axis=1)
-  
     return matrix_data, y_desired, weights, matrix_data_w0
 
 def create_weights(num_weights):
-    return [1] + [random.uniform(0, 1) for _ in range(num_weights - 1)]
+    return [random.uniform(0, 1) for _ in range(num_weights)]
 
 def step_function(x):
     return np.where(x >= 0, 1, 0)
@@ -36,44 +37,41 @@ def step_function(x):
 def perceptron_output(features, weights, bias):
     u = np.dot(features, weights) + bias
     y_calculada = step_function(u)
-    
-    return u, y_calculada
+    return u, np.array(y_calculada)
 
-name_file = './data/E2.csv'
+def calculate_delta(learning_rate, error_y, features):
+    error_y_array = np.array(error_y).reshape(-1, 1)
+    return learning_rate * np.dot(error_y_array.T, features)
+
+
 matrix_data, y_desired, weights, matrix_data_w0 = read_data(name_file)
 
 for epoch in range(epochs):
+    error_y = []  
     print(f"\nEpoch {epoch + 1}:")
-    
-   
-    delta_weights = np.zeros_like(weights[epoch])
-    
     for i in range(len(matrix_data_w0)):
         u, y_calculada = perceptron_output(matrix_data_w0[i], weights[epoch], bias)
+        y_calculada = np.array(y_calculada)
         error = y_desired[i] - y_calculada
         error_norm = np.linalg.norm(error)
         u_values.append(u)
         y_calculadas.append(y_calculada)
+        error_y.append(error)
         error_norms.append(error_norm)
-
         
-        delta_weights += learning_rate * error * matrix_data_w0[i]
-
         print(f"Conjunto de datos {i + 1}:")
         print("  Entradas:", matrix_data_w0[i])
         print("  Pesos:", weights[epoch])
         print("  y calculada:", y_calculada)
         print("  Error:", error)
         print("  Norma del error:", error_norm)
+        print(f"  Suma de u para el conjunto {i + 1}: {u}")
+        
+    delta_weights = calculate_delta(learning_rate, error_y, matrix_data_w0)
+    print("  Delta Weights:", delta_weights)
 
-   
-    weights[epoch] += delta_weights
-
-    print(f"\nDelta weights en la época {epoch + 1}: {delta_weights}")
-    print(f"Pesos actualizados en la época {epoch + 1}: {weights[epoch]}")
-
-print("\nPesos finales:")
-for epoch, peso in enumerate(weights):
-    print(f"Iteración {epoch + 1}: {peso}")
+    weights[epoch] += delta_weights  
+    print("Nuevos pesos ", weights[epoch])
+    bias += learning_rate * np.sum(error_y)
 
 print("Sesgo final:", bias)
